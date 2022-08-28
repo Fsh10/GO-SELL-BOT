d17ccff262f351e41d255d4810e614ea2b748982
fedor.shtab <fedor.shtab@gmail.com>
2022-08-28 10:04:00 +0400

    Add GO-SELL API client

---
+++ bot/desc/product.go
@@ -0,0 +1,69 @@
+package desc
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Fsh10/GO-SELL-BOT/bot/digi"
+)
+
+type (
+	Products struct {
+		Items []Product `json:"product"`
+		Pages string    `json:"totalPages"`
+	}
+
+	Product struct {
+		Id      string
+		Name    string
+		Info    string `json:"-"`
+		AddInfo string `json:"-"`
+		Price   string `json:"price_rub"`
+		Curr    string `json:"base_currency"`
+	}
+
+	ProductFull struct {
+		Product struct {
+			Info    string `json:"info"`
+			AddInfo string `json:"add_info"`
+		}
+	}
+)
+
+var (
+	htmlReplacements = map[string]string{
+		"<br />":       "\n",
+		"<br/>":        "\n",
+		"<br>":         "\n",
+		"<attention>":  "<b>",
+		"</attention>": "</b>",
+		"<delivery>":   "<i>",
+		"</delivery>":  "</i>",
+	}
+)
+
+func (p Product) String() string {
+	imageUrl := digi.ProductImageUrl(p.Id)
+
+	name := p.Name
+	info := p.Info
+	for k, v := range htmlReplacements {
+		name = strings.ReplaceAll(name, k, v)
+		info = strings.ReplaceAll(info, k, v)
+	}
+
+	res := fmt.Sprintf("%s\n\n%s\n\n%s %s\n<a href='%s'>&#8205;</a>", name, info, p.Price, "RUB", imageUrl)
+	return res
+}
+
+func (p Product) Instruction() string {
+	addInfo := p.AddInfo
+	for k, v := range htmlReplacements {
+		addInfo = strings.ReplaceAll(addInfo, k, v)
+	}
+	return addInfo
+}
+
+func (p Product) PaymentURL(sellerId string) string {
+	return digi.ProductPaymentURL(p.Id, sellerId)
+}
---
+++ bot/digi/endpoints.go
@@ -0,0 +1,52 @@
+package digi
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	uhttp "github.com/Fsh10/GO-SELL-BOT/utils/http"
+)
+
+const (
+	ReviewsLogoUrl = "https://my.GO-SELL.ru/preview/257605/logo_20220513174904.png"
+	CategoryUrl    = "https://api.GO-SELL.ru/api/categories?seller_id=%s"
+	ProductListUrl = "https://api.GO-SELL.ru/api/shop/products?seller_id=%s&category_id=%s&page=%d"
+	ProductDataUrl = "https://api.GO-SELL.ru/api/products/%s/data"
+
+	imageUrlTempl       = "http://graph.GO-SELL.ru/img.ashx?id_d=%s&maxlength=400"
+	paymentURLTempl     = "https://www.oplata.info/asp2/pay_api.asp?id_d=%s&curr=API_5011_RUB&_subcurr=&lang=ru-RU&_ids_shop=%s&failpage=https://x-box-store.ru"
+	purchasesOptionsUrl = "https://api.GO-SELL.ru/api/purchases/options"
+)
+
+func CustomProductPaymentURL(
+	client *http.Client,
+	sellerId, productName string,
+	paymentProductID, paymentProductOption, price int) string {
+
+	if paymentProductID != 0 {
+		ppo := ProductPurchasesOptions{
+			ProductID: paymentProductID,
+			UnitCnt:   price,
+			IP:        "127.0.0.1",
+		}
+		ppo.Options = append(ppo.Options, ProductPurchasesOption{
+			ID:    paymentProductOption,
+			Value: PPOValue{Text: productName}})
+		if info, pErr := uhttp.Post[ProductPaymentData](client, purchasesOptionsUrl, ppo); pErr == nil {
+			if info.Retval == 0 {
+				PaymentProductIDString := strconv.Itoa(paymentProductID)
+				return fmt.Sprintf(paymentURLTempl, PaymentProductIDString, sellerId) + fmt.Sprintf("&id_po=%d", info.IDPo)
+			}
+		}
+	}
+	return ""
+}
+
+func ProductPaymentURL(productID, sellerID string) string {
+	return fmt.Sprintf(paymentURLTempl, productID, sellerID)
+}
+
+func ProductImageUrl(id string) string {
+	return fmt.Sprintf(imageUrlTempl, id)
+}
